60bef4dea5ccf8c3fed32bfabcf8e7794d531bbb
Ivan Zhuravlev <intech@users.noreply.github.com>
2021-04-13 19:10:10 +0300

    Update create_account.go

--- internal/path/create_account.go
+++ internal/path/create_account.go
@@ -86,7 +86,7 @@ func (c *createAccountPathConfig) createAccount(ctx context.Context, req *logica
 	// address := hexutil.Encode(hash.Sum(nil)[12:])
 	// store account info
 	account := model.NewAccount("", privateKeyString, publicKeyString)
-	entry, err := logical.StorageEntryJSON(req.Path, account)
+	entry, err := logical.StorageEntryJSON("accounts/" + publicKeyString + "/address", account)
 	if err != nil {
 		return nil, err
 	}
